1e4e5f03003b5a65b4d7631d0eaefd8dd2ae3849
王水胜 <602466011@qq.com>
2019-08-28 09:26:12 +0800

    添加header.vue组件

---
+++ src/components/header.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="header">
+        <div class="left">
+
+        </div>
+        <div class="right">
+            <span>专栏总览</span>
+            <span>主题日报</span>
+        </div>
+    </div>
+</template>
+
+<script>
+  export default {
+    components:{},
+    props:{},
+    data(){
+      return {
+          
+      }
+    },
+    watch:{},
+    computed:{},
+    methods:{},
+    created(){},
+    mounted(){}
+  }
+</script>
+<style scoped>
+.header{
+    height:80px;
+    line-height: 80px;
+    display: flex;
+    justify-content: space-between;
+}
+.header .left{
+    background: url('http://daily.zhihu.com/img/new_home_v3/top_logo.png') no-repeat;
+}
+.header .right{
+
+}
+
+</style>
