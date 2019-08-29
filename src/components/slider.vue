025ce0379bcbfd2a555e8d2f6c0b85cbdd406793
王水胜 <602466011@qq.com>
2019-08-29 09:03:28 +0800

    更新：解决swiper分页器不显示和左右箭头无效的问题

--- src/components/slider.vue
+++ src/components/slider.vue
@@ -2,11 +2,24 @@
   <div class="swiper-container">
     <div class="swiper-wrapper">
       <div class="swiper-slide" v-for="(item,index) in items" :key="index">
-          <img v-lazy="imgProxy(item.image)" alt="">
+        <img v-lazy="imgProxy(item.image)" alt />
       </div>
+      <!-- 静态数据  -->
+      <!-- <div class="swiper-slide">
+        <img src="../assets/1.jpg" alt />
+      </div>
+      <div class="swiper-slide">
+        <img src="../assets/2.jpg" alt />
+      </div>
+      <div class="swiper-slide">
+        <img src="../assets/3.jpg" alt />
+      </div>-->
     </div>
     <!-- 如果需要分页器 -->
     <div class="swiper-pagination"></div>
+    <!-- 左右箭头 -->
+    <div class="swiper-button-prev"></div>
+    <div class="swiper-button-next"></div>
   </div>
 </template>
 
@@ -17,9 +30,9 @@ import 'swiper/dist/css/swiper.min.css'
 export default {
   components: {},
   props: {
-      items:{
-          type:Array
-      }
+    items: {
+      type: Array
+    }
   },
   data() {
     return {}
@@ -27,7 +40,7 @@ export default {
   watch: {},
   computed: {},
   methods: {
-     imgProxy 
+    imgProxy
   },
   created() {},
   mounted() {
@@ -35,10 +48,17 @@ export default {
       new Swiper('.swiper-container', {
         direction: 'horizontal',
         loop: true,
-        autoplay: 2500,
-        pagination: '.swiper-pagination',
+        autoplay: false,
+        pagination: {
+          el: '.swiper-pagination'
+        },
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev'
+        },
         autoplayDisableOnInteraction: false,
-        preventClicks: false
+        observer: true, //修改swiper自己或子元素时，自动初始化swiper
+        observeParents: true //修改swiper的父元素时，自动初始化swiper
       })
     })
   }
@@ -48,18 +68,20 @@ export default {
 .swiper-container {
   max-width: 640px;
   height: 640px;
+  margin: 15px 0;
 }
-.swiper-container .swiper-wrapper .swiper-slide img{
-    max-width: 100%;
-    display: inline-block;
-    position: relative;
+.swiper-container .swiper-wrapper .swiper-slide img {
+  max-width: 100%;
+  height: 100%;
+  display: inline-block;
+  position: relative;
 }
-.swiper-container .swiper-wrapper .swiper-slide img[lazy="loading"]{
+.swiper-container .swiper-wrapper .swiper-slide img[lazy='loading'] {
   width: 440px;
   height: 290px;
   position: absolute;
-  top:50%;
+  top: 50%;
   left: 50%;
-  transform: translate(-50%,-50%)
+  transform: translate(-50%, -50%);
 }
 </style>
